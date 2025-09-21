8db62ccf2d367be172453251f049dbd0809d11b7
Derpitron <derpitron1@proton.me>
2025-09-21 23:34:42 +0530

    [wip] get new config file format in place

--- scripts/Windows/setup.bat
+++ scripts/Windows/setup.bat
@@ -3,8 +3,11 @@ echo Installing requirements...
 pip install -r "%~dp0..\..\requirements.txt"
 echo Requirements installed!
 
-start notepad.exe "%~dp0..\..\user\cfg.yml"
-echo Setup the cfg.yml file. 
+start notepad.exe "%~dp0..\..\config\account.yml"
+echo Setup the your account details here.
+
+start notepad.exe "%~dp0..\..\config\program.yml"
+echo Setup the program configuration you want here here. 
 
 :LOOP
 tasklist | find /i "notepad.exe" > nul
