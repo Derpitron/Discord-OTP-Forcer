e9c519b810063b7f0367db57c953399479a6c831
Derpitron <derpitron1@proton.me>
2025-12-01 18:15:46 +0530

    update formatting

--- src/lib/codegen.py
+++ src/lib/codegen.py
@@ -1,55 +1,33 @@
-from string import digits, ascii_lowercase as letters
 from secrets import choice as randchoice
+from string import ascii_lowercase as letters
+from string import digits
 
-# All the functions in the main file for simplicity's sake
+
 def list_to_string(l: list) -> str:
-	"""
-	A simple function that turns a list into a string. Made this so i don't repeat myself...
+    return "".join(map(str, l))
 
-	Args:
-
-		list: list to be formatted into a string.
-
-	Returns:
-		All list elements concatenated to one string
-	"""
-	return ''.join(map(str, l))
 
 def generate_random_character(character_set: str) -> str:
-	"""
-	Generate a random character from the given character set.
+    yield randchoice(character_set)
 
-	:param character_set: a string containing the characters to choose from
-	:type character_set: str
-	:return: a single randomly selected character from the character set
-	:rtype: str
-	"""
-	yield randchoice(character_set)
 
 def generate_random_code(mode: str) -> str:
-	"""
-	Generate a code based on the given mode parameter.
-
-	:param mode: A string representing the mode of code generation. It can be either 'normal' or 'backup'.
-	:type mode: str or None
-
-	:return: A string representing the generated code.
-	:rtype: str or None
-
-	"""
-	# The nitty-gritty settings for code generation based on mode.
-	match mode:
-		case 'normal':
-			# Generates a 6-digit numeric 'normal' code
-			return list_to_string( list( next(generate_random_character(           digits )) for _ in range(6)))
-		case 'backup':
-			# Generates an 8-digit alphanumeric 'backup' code
-			return list_to_string( list( next(generate_random_character( letters + digits )) for _ in range(8)))
-		case 'backup_let':
-			# Generates an 8-digit alphabetical 'backup' code
-			return list_to_string( list( next(generate_random_character( letters )) for _ in range(8)))
-		case 'both':
-			# Generates a code with a random possibility of being 'normal' or 'backup' type
-			# We pack the two generate_random_code options into a list, pass as a single parameter to randchoice
-			# Source: https://stackoverflow.com/a/70510607/19195633
-			return randchoice( [generate_random_code('normal'), generate_random_code('backup')] )
+    match mode:
+        case "normal":
+            return list_to_string(
+                list(next(generate_random_character(digits)) for _ in range(6))
+            )
+        case "backup":
+            return list_to_string(
+                list(
+                    next(generate_random_character(letters + digits)) for _ in range(8)
+                )
+            )
+        case "backup_let":
+            return list_to_string(
+                list(next(generate_random_character(letters)) for _ in range(8))
+            )
+        case "both":
+            return randchoice(
+                [generate_random_code("normal"), generate_random_code("backup")]
+            )
--- src/lib/textcolor.py
+++ src/lib/textcolor.py
@@ -1,30 +1,16 @@
 def color(string: str, color: str) -> str:
-	"""
-	Given a string and a color, return the string with the specified color code applied using ANSI escape codes.
+    color_code_map = {
+        "red": "\033[31m",
+        "yellow": "\033[33m",
+        "green": "\033[32m",
+        "cyan": "\033[96m",
+        "blue": "\033[34m",
+        "magenta": "\033[35m",
+        "black": "\033[30m",
+        "white": "\033[37m",
+        "custom": "\033[0;34;47m",
+    }
+    if color not in color_code_map:
+        raise ValueError("Invalid ANSI color code choice.")
 
-	:param string: The string to colorize.
-	:type string: str or None
-
-	:param color: The color to use for the string. Must be one of 'red', 'yellow', 'green', 'cyan', 'blue', 'magenta', 'black', 'white', or 'custom'.
-	:type color: str or None
-
-	:return: The input string with the specified color applied using ANSI escape codes, or None if either the input string or color is None.
-	:rtype: str or None
-
-	:raises ValueError: If the input color is not one of the supported colors.
-	"""
-	color_code_map = {
-		    'red' : '\033[31m',
-		 'yellow' : '\033[33m',
-		  'green' : '\033[32m',
-		   'cyan' : '\033[96m',
-		   'blue' : '\033[34m',
-		'magenta' : '\033[35m',
-		  'black' : '\033[30m',
-		  'white' : '\033[37m',
-		 'custom' : '\033[0;34;47m'
-	}
-	if color not in color_code_map: raise ValueError('Invalid ANSI color code choice.')
-
-
-	return f"{color_code_map[color]}{string}\033[0m"
+    return f"{color_code_map[color]}{string}\033[0m"
