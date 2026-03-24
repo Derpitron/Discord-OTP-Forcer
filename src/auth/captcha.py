dd0e3d263ff0cbcea96cba70eb5fe5658d50c023
inbydev <a80e2517-1246-49a5-907a-c38b8c2c97fb.1yg40@silomails.com>
2026-03-24 20:44:55 -0300

    refactor(captcha): Added support to use BrowserSession, optimized some comparations and used explicit wait instead of implicit wait

--- src/auth/captcha.py
+++ src/auth/captcha.py
@@ -4,22 +4,27 @@ from loguru import logger
 
 from selenium.webdriver.common.by import By, ByType
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+from src.lib.types import BrowserSession
 
 
-def captcha_detection(
-    driver: WebDriver,
-) -> None:
+def captcha_detection(session: BrowserSession) -> None:
+    driver, config = session.driver, session.config
+
     captcha_box: tuple[ByType, str] = (By.CLASS_NAME, "container__8a031")
-    captcha_detected: bool = bool(driver.find_elements(*captcha_box))
+    wait: WebDriverWait[WebDriver] = WebDriverWait(driver, config.program.elementLoadTolerance)
 
-    if not captcha_detected:
-        logger.debug("No captcha detected. Moving on to the rest of the script.")
-        return
-
-    while True:  # Check if the captcha_box still exists
+    try:
+        wait.until(EC.presence_of_element_located(captcha_box))
         logger.info("A captcha detected. Please complete the captcha for the program to continue.")
-        time.sleep(1)
-        if not driver.find_elements(*captcha_box):  # Waits for the program to detect that the captcha isn't there.
-            break
 
-    logger.debug("Captcha has been completed. Moving on to the rest of the script.")
+        while driver.find_elements(*captcha_box):
+            time.sleep(1)
+
+        logger.debug("Captcha has been completed. Moving on to the rest of the script.")
+
+    except TimeoutException:
+        logger.debug("No captcha detected. Moving on to the rest of the script.")
