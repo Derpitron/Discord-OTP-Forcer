457a2ff978d5747498d2df6e3da4917cd7ffa02f
inbydev <a80e2517-1246-49a5-907a-c38b8c2c97fb.1yg40@silomails.com>
2026-05-09 16:10:23 -0400

    chore(ConfigParser): Moved config parsing logic from main.py to src/config/config_parser.py Python module file

--- main.py
+++ main.py
@@ -1,126 +1,14 @@
-import sys
-import time
-from time import strftime
-
-from loguru import logger
-from yaml import safe_load as load
-
 from src.backend import bootstrap_browser, bootstrap_code_page, try_codes
-from src.lib.logcreation import formatter, formatter_sensitive
+from src.config.config_parser import load_configuration
+from src.logger.log_init import initialize_logger, initialize_check_logger
 from src.lib.types import (
-    AccountConfig,
-    Browser,
     BrowserSession,
-    CodeMode_Backup,
-    CodeMode_Normal,
-    Config,
-    ProgramConfig,
-    ProgramMode,
-    ProgramConfigDict,
-    CensoredStr,
 )
 
-
-def load_configuration(account_config_path: str, program_config_path: str) -> Config:
-    """
-    Parses the config files to our python object.
-    """
-
-    accountConfig: AccountConfig
-    programConfig: ProgramConfig
-
-    # We first read the program_config_file so we know if sensitiveDebug is active or not
-    with open(program_config_path, "r") as program_config_file:
-        program_config_dict: ProgramConfigDict = load(program_config_file)
-
-    # And if it is, we convert the strings into the CensoredStr type
-    with open(account_config_path, "r") as account_config_file:
-        account_dict = load(account_config_file)
-
-        if program_config_dict["sensitiveDebug"]:
-            censored_account_dict = {}
-            for key, value in account_dict.items():
-                if value:
-                    value = CensoredStr(value)
-                censored_account_dict[key] = value
-
-            accountConfig = AccountConfig(**censored_account_dict)
-        else:
-            accountConfig = AccountConfig(**account_dict)
-
-    check_updates: bool | None = program_config_dict.get("checkUpdates")
-
-    # need a custom parser for this cus of custom types.
-    # If the user gives a custom regex here i'll assume it's a backup code.
-    # fmt: off
-    programConfig = ProgramConfig(
-        programMode=ProgramMode[(program_config_dict["programMode"])],
-
-        codeMode=(
-            CodeMode_Normal()
-            if program_config_dict["codeMode"] == "Normal"
-            else (
-                CodeMode_Backup()
-                if program_config_dict["codeMode"] == "Backup"
-                else CodeMode_Backup(program_config_dict["codeMode"])
-            )
-        ),
-        checkUpdates=check_updates if check_updates is not None else False,
-        browser=Browser[(program_config_dict["browser"])],
-        headless=program_config_dict["headless"],
-        logCreation=program_config_dict["logCreation"],
-        sensitiveDebug=program_config_dict["sensitiveDebug"],
-        logLevel=program_config_dict["logLevel"],
-        elementLoadTolerance=program_config_dict["elementLoadTolerance"],
-        usualAttemptDelayRange=(
-            program_config_dict["usualAttemptDelayMin"],
-            program_config_dict["usualAttemptDelayMax"],
-        ),
-        ratelimitedAttemptDelayRange=(
-            program_config_dict["ratelimitedAttemptDelayMin"],
-            program_config_dict["ratelimitedAttemptDelayMax"],
-        ),
-    )
-    # fmt: on
-
-    formatting = formatter
-    if programConfig.sensitiveDebug:
-        formatting = formatter_sensitive
-    if programConfig.logCreation:
-        # fmt: off
-        logger.add(
-            "log/{0}.log".format(
-                strftime("%d-%m-%Y-%H_%M_%S", time.localtime(time.time()))
-            ),
-            colorize=False,
-            backtrace=True,
-            format=formatting,
-        )
-        # fmt: on
-
-    logger.add(
-        sys.stderr,
-        format=formatting,
-        colorize=True,
-        backtrace=True,
-        level=programConfig.logLevel,
-    )
-
-    if check_updates is None:
-        logger.warning(
-            "Your configuration is not up to date. Please use the updated config at: https://codeberg.org/Discord-OTP-Forcer/Discord-OTP-Forcer/src/branch/main/config/program.yml"
-        )
-        logger.warning("Missing configuration: 'checkUpdates' - Defaulting to False since the configuration doesn't exist.")
-
-    logger.debug("Loaded config/account.yml and config/program.yml config files.")
-
-    return Config(account=accountConfig, program=programConfig)
-
-
 if __name__ == "__main__":
-    logger.remove()
-
-    config: Config = load_configuration("config/account.yml", "config/program.yml")
+    initialize_check_logger()
+    config, check_updates_missing = load_configuration("config/account.yml", "config/program.yml")
+    initialize_logger(config.program, check_updates_missing)
     session: BrowserSession | None = None
 
     if config.program.checkUpdates:
---
+++ src/config/config_parser.py
@@ -0,0 +1,83 @@
+from yaml import safe_load as load
+from src.config.config_validator import validate_program_config, validate_account_config
+from src.lib.types import (
+    AccountConfig,
+    Browser,
+    CodeMode_Backup,
+    CodeMode_Normal,
+    Config,
+    ProgramConfig,
+    ProgramMode,
+    ProgramConfigDict,
+    CensoredStr,
+)
+
+
+def load_configuration(account_config_path: str, program_config_path: str) -> tuple[Config, bool]:
+    """
+    Parses the config files into our Python objects.
+    Validation (syntax + values) is done before any parsing attempt.
+    """
+    accountConfig: AccountConfig
+    programConfig: ProgramConfig
+
+    validate_program_config(program_config_path)
+    # account validation needs programMode, so we read program config first
+    with open(program_config_path, "r") as _f:
+        _mode: str = load(_f).get("programMode", "")
+    validate_account_config(account_config_path, _mode)
+
+    # We first read the program_config_file so we know if sensitiveDebug is active or not
+    with open(program_config_path, "r") as program_config_file:
+        program_config_dict: ProgramConfigDict = load(program_config_file)
+
+    # And if it is, we convert the strings into the CensoredStr type
+    with open(account_config_path, "r") as account_config_file:
+        account_dict = load(account_config_file)
+
+        if program_config_dict["sensitiveDebug"]:
+            censored_account_dict = {}
+            for key, value in account_dict.items():
+                if value:
+                    value = CensoredStr(value)
+                censored_account_dict[key] = value
+
+            accountConfig = AccountConfig(**censored_account_dict)
+        else:
+            accountConfig = AccountConfig(**account_dict)
+
+    check_updates: bool | None = program_config_dict.get("checkUpdates")
+
+    # need a custom parser for this cus of custom types.
+    # If the user gives a custom regex here i'll assume it's a backup code.
+    # fmt: off
+    programConfig = ProgramConfig(
+        programMode=ProgramMode[(program_config_dict["programMode"])],
+
+        codeMode=(
+            CodeMode_Normal()
+            if program_config_dict["codeMode"] == "Normal"
+            else (
+                CodeMode_Backup()
+                if program_config_dict["codeMode"] == "Backup"
+                else CodeMode_Backup(program_config_dict["codeMode"])
+            )
+        ),
+        checkUpdates=check_updates if check_updates is not None else False,
+        browser=Browser[(program_config_dict["browser"])],
+        headless=program_config_dict["headless"],
+        logCreation=program_config_dict["logCreation"],
+        sensitiveDebug=program_config_dict["sensitiveDebug"],
+        logLevel=program_config_dict["logLevel"],
+        elementLoadTolerance=program_config_dict["elementLoadTolerance"],
+        usualAttemptDelayRange=(
+            program_config_dict["usualAttemptDelayMin"],
+            program_config_dict["usualAttemptDelayMax"],
+        ),
+        ratelimitedAttemptDelayRange=(
+            program_config_dict["ratelimitedAttemptDelayMin"],
+            program_config_dict["ratelimitedAttemptDelayMax"],
+        ),
+    )
+    # fmt: on
+    return Config(account=accountConfig, program=programConfig), check_updates is None
